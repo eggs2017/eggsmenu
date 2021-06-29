a5e11253ae47eaba8e978a5c0943c0d652f93cec
eggs2017 <32611063+eggs2017@users.noreply.github.com>
2021-06-29 16:59:23 +0200

    uuidv4

--- routes/reserveNick.js
+++ routes/reserveNick.js
@@ -122,7 +122,14 @@ router.get('/', function (req, res, next) {
               });
             });
           }
-
+			
+			function uuidv4() {
+          return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+          });
+        }
+		
             //Promises
 			if(hasLdap){
             Promise.all(
@@ -155,9 +162,11 @@ router.get('/', function (req, res, next) {
 				});
 			}
 			else{
+				
+				
 				res.send( JSON.stringify(
-                                { nick: vipName[0].name,
-                                  person: vipName[0].prefix + " " + vipName[0].name
+                                { nick: vipName[0].name+ "-"+uuidv4(),
+                                  person: vipName[0].prefix + " " + vipName[0].name+ "-"+uuidv4()
                                 } )
                               );
 			}
